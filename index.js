8a532c0928b6d0f258e51a902b9e0c74786b8513
sanzdecastro <sanzdecastro@gmail.com>
2024-04-11 12:58:52 +0200

    fixes

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 function change() {
     let title = document.querySelector(".titleClassName");
-
+    
+    // let newColor = "green";
+    // title.style.color = newColor;
+    
     let colors = ["green", "red", "blue", "black"];
     let randomColor = Math.floor(Math.random() * colors.length);
 
