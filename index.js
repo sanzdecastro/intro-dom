efc35f12d37e7db6bc18eb699bb85844e3f0ab87
sanzdecastro <sanzdecastro@gmail.com>
2024-04-11 12:19:19 +0200

    ex1

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
-function change() {
-    // your code goes here
-}
+// const buttonElement = document.getElementById("button");
+// buttonElement.addEventListener("click", function change() {
+//     let titleh1 = document.querySelector("h1").textContent;
+//     console.log(titleh1);
+// });
+
+let buttonElement = document.querySelector(".button");
+buttonElement.addEventListener("click", function change() {
+    let titleh1 = document.querySelector("h1").textContent;
+    console.log(titleh1);
+});
