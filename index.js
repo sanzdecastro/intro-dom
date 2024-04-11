489ae84329acb485b4ae837593fa4b1f59fa0654
sanzdecastro <sanzdecastro@gmail.com>
2024-04-11 12:48:26 +0200

    fixes

--- index.js
+++ index.js
@@ -4,8 +4,15 @@
 //     console.log(titleh1);
 // });
 
-let buttonElement = document.querySelector(".button");
-buttonElement.addEventListener("click", function change() {
-    let titleh1 = document.querySelector("h1").textContent;
-    console.log(titleh1);
-});
+// let buttonElement = document.querySelector(".button");
+// buttonElement.addEventListener("click", function change() {
+//     let titleh1 = document.querySelector("h1").textContent;
+//     console.log(titleh1);
+// });
+
+function change() {
+    // let h1Title = document.querySelector("h1").textContent;
+    // let h1Title = document.querySelector(".titleName").textContent;
+    const h1Title = document.getElementById("titleId").textContent;
+    console.log(h1Title);
+}
