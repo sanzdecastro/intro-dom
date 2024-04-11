1d8793761215b3f8d3fe91d704403aa9f33a1787
sanzdecastro <sanzdecastro@gmail.com>
2024-04-11 12:31:06 +0200

    ex2

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
 function change() {
-    // your code goes here
+    let title = document.querySelector(".titleClassName");
+
+    let colors = ["green", "red", "blue", "black"];
+    let randomColor = Math.floor(Math.random() * colors.length);
+
+    title.style.color = colors[randomColor];
 }
