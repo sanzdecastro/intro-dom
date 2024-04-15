9d3ff7ec6f40287e3ffaec57f75edb04580c478c
sanzdecastro <sanzdecastro@gmail.com>
2024-04-15 10:38:21 +0200

    option2

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 function change() {
     // add red squares to the HTML when the button is clicked
-    let squareContainer = document.querySelector("article");
-    let newSquare = document.createElement("div");
-
-    squareContainer.appendChild(newSquare).classList.add("redSquare");
+    let squareContainer = document.querySelector(".squaresContainer");
+    // let newSquare = document.createElement("div");
+    let squareHTML = "<div class='redSquare'></div>";
+    //squareContainer.appendChild(newSquare).classList.add("redSquare");
+    squareContainer.innerHTML += squareHTML;
 }
